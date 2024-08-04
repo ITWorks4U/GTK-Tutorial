1cf46130d381ec4253929bb5cbe8628bbd054530
ITWorks4U <itworks4U@gmail.com>
2024-08-04 20:08:38 +0200

    deal with cairo vector graphics

--- GTK_C/11_drawning/1_become_an_artist/source/gtk_event_listeners.c
+++ GTK_C/11_drawning/1_become_an_artist/source/gtk_event_listeners.c
@@ -1,9 +1,19 @@
+/*
+	Draw your own art on a GTKDrawArea.
+	Maybe you have luck and your art will be
+	shown in the Louvre? :o)
+
+	(C) 2024 ITWorks4U
+*/
+
 #include "../header/gtk_full_tutorial.h"
+#include <string.h>
 
 void window_destroy(void) {
 	gtk_main_quit();
 }
 
+/*	on very window resize or when the right mouse button has been pressed	*/
 void clear_surface(void) {
 	cairo_t *cr = cairo_create(surface);
 	cairo_set_source_rgb(cr, 1.0, 1.0, 1.0);
@@ -11,13 +21,31 @@ void clear_surface(void) {
 	cairo_destroy(cr);
 }
 
+/*	event function for GTKDrawArea	*/
 void draw_brush(GtkWidget *widget, gdouble x, gdouble y) {
+	/*	the surface object needs to be created here	*/
 	cairo_t *cr = cairo_create(surface);
-	cairo_rectangle(cr, x, y, 10.0, 10.0);
-	cairo_fill(cr);
+
+	/*	setting the selected color for the object to draw	*/
+	cairo_set_source_rgba(cr, color.red, color.green, color.blue, color.alpha);
+
+	/*	time to paint	*/
+	if (is_rectangle) {
+		cairo_rectangle(cr, x, y, size_x, size_y);
+	} else {
+		//	surface, x, y, radius <=> size_x, start angle, end angle
+		cairo_arc(cr, x, y, size_x, 0.0, 2 * PI);
+	}
+
+	if (on_stroke) {
+		cairo_stroke(cr);
+	} else {
+		cairo_fill(cr);
+	}
+	
 	cairo_destroy(cr);
 
-	gtk_widget_queue_draw_area(widget, x, y, 10.0, 10.0);
+	gtk_widget_queue_draw_area(widget, x, y, size_x, size_y);
 }
 
 gboolean configure_event_cb(GtkWidget *widget, GdkEventConfigure *event, gpointer data) {
@@ -63,4 +91,41 @@ gboolean motion_notify_event_cb (GtkWidget *widget, GdkEventMotion *event, gpoin
 	}
 
 	return TRUE;
+}
+
+void changed_style_handling(GtkComboBoxText *cboBox, AppWidgets *app) {
+	/*	just differencing between a rectangle and a circle	*/
+	if (strncmp(gtk_combo_box_text_get_active_text(app->cbo_style), RECTANGLE, strlen(RECTANGLE)) == 0) {
+		is_rectangle = true;
+	} else {
+		is_rectangle = false;
+	}
+}
+
+void toggle_object_fill(void) {
+	static int ctr = 0;
+	ctr++;
+
+	if (ctr % 2 == 1) {
+		on_stroke = true;
+	} else {
+		on_stroke = false;
+	}
+}
+
+void spin_x_changed(GtkSpinButton *btn, AppWidgets *app) {
+	size_x = gtk_spin_button_get_value_as_int(app->spin_x);
+}
+
+void spin_y_changed(GtkSpinButton *btn, AppWidgets *app) {
+	size_y = gtk_spin_button_get_value_as_int(app->spin_y);
+}
+
+void choosen_color(GtkColorButton *btn, AppWidgets *app) {
+	gtk_color_chooser_get_rgba(GTK_COLOR_CHOOSER(app->color_btn), &color);
+
+	/*
+		just nice to know which color is in use now
+		g_print("red: %.2lf, green: %.2lf, blue: %.2lf, alpha: %.2lf\n", color.red, color.green, color.blue, color.alpha);
+	*/
 }
